5cd423b92c9f987a1f259abbf6882270f0b3dcfd
SeigoKidoguchi <toonlink4002@yahoo.co.jp>
2023-01-10 14:52:04 +0900

    change one word

--- main.go
+++ main.go
@@ -16,12 +16,12 @@ func main(){
 
 func Hello() echo.HandlerFunc{
 	return func(c echo.Context) error{
-		return c.String(http.StatusOK, "4417:iruma")
+		return c.String(http.StatusOK, "4417:Kamelot")
 	}
 }
 
 func ApiHelloGet() echo.HandlerFunc{
 	return func(c echo.Context) error{
-		return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4417", "message": "iruma"})
+		return c.JSON(http.StatusOK, map[string]interface{}{"studentId": "4417", "message": "Kamelot"})
 	}
 }
